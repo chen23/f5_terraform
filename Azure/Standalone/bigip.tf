16c0548e0ccf733c809d2d3921d91f0e1e2a99c9
Jeff <thejeffgiroux@gmail.com>
2020-09-04 19:39:35 -0700

    remove privatevip variable

--- Azure/Standalone/bigip.tf
+++ Azure/Standalone/bigip.tf
@@ -124,7 +124,6 @@ data "template_file" "as3" {
     client_id       = var.sp_client_id
     client_secret   = var.sp_client_secret
     publicvip       = var.f5publicvip
-    privatevip      = var.f5privatevip
   }
 }
 
